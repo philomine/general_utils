1568530c861957804724178f4e786e7599bfdc82
Philomene <philomene.bobichon@gmail.com>
2019-06-20 11:24:07 +0200

    Forgot parenthesis in progress_bar function

--- custom_functions.py
+++ custom_functions.py
@@ -21,6 +21,7 @@ def clear_nans(X, dont_drop=None):
 
 def progress_bar(iteration, total, length=50, empty_character=' ', fill_character='=', title='Progress'):
     '''
+    Careful: iteration is meant to go from 0 to total-1, just like in the example
     :Example:
     >>> for i, item in enumerate(items):
     >>>     progress_bar(i, len(items))
@@ -28,15 +29,15 @@ def progress_bar(iteration, total, length=50, empty_character=' ', fill_characte
     Progress: [==================================================] 100.0%
     '''
     # Computing useful values
-    percent = ("{0:5.1f}").format(100 * (iteration+1 / float(total)))
-    filled_length = int(length * iteration+1 // total)
+    percent = ("{0:5.1f}").format(100 * ((iteration+1) / float(total)))
+    filled_length = int(length * (iteration+1) // total)
     bar = fill_character * filled_length + empty_character * (length - filled_length)
 
     # Updating printed output
     print('%s: [%s] %s%% ' % (title, bar, percent), end='\r')
 
     # Printing a new line at the end of the loop
-    if iteration == total:
+    if iteration+1 == total:
         print()
 
     return None
