9ce5824bb27ec406b921bdb62cf61636abfe1b25
Philomène Bobichon <philomene.bobichon@gmail.com>
2019-10-22 11:16:33 +0200

    add_method decorator to dynamically add a method to a class

--- custom_functions.py
+++ custom_functions.py
@@ -46,3 +46,25 @@ def progress_bar(i, n):
         print("")
 
     return None
+
+# Medium article "Dynamically add a method to a class" by Michael Garod
+# https://medium.com/@mgarod/dynamically-add-a-method-to-a-class-in-python-c49204b85bd6
+def add_method(cls):
+    '''
+    Add a method to a class.
+
+    :Example: 
+    # Adding method foo() to class 
+    @add_method(A)
+    def foo():
+        print('hello world!')
+    # Method foo() can still be used on its own (doesn't need self attribute).
+    '''
+    def decorator(func):
+        @wraps(func) 
+        def wrapper(self, *args, **kwargs): 
+            return func(*args, **kwargs)
+        setattr(cls, func.__name__, wrapper)
+        # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
+        return func # returning func means func can still be used normally
+    return decorator
