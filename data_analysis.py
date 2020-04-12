12354db8e3d51438f1c552d4970e5520c16fdfe0
Philomene Bobichon <philomene.bobichon@konecranes.com>
2020-04-12 18:49:10 +0200

    Preparing for merge into branch

--- data_analysis.py
+++ data_analysis.py
@@ -1,68 +1,31 @@
-# import sys
-# import os
-# sys.path.append(os.path.expanduser("~/Projects/Python/python_custom_functions"))
+import string
 
 import numpy as np
 import pandas as pd
-import time
-import datetime
-import string
-from scipy import stats
-from functools import wraps # This convenience func preserves name and docstring
 
-import plotly
-import plotly.express as px
-import plotly.graph_objects as go
-
-def number_of_nan(X, axis = 0):
-    X = np.array(X, dtype = 'float')
-    return np.sum(np.isnan(X), axis=axis)
-
-def clear_nans(X, dont_drop=None):
-    number_of_nans_in_columns = number_of_nan(X, axis=0)
-    if not len(np.unique(number_of_nans_in_columns)) == 1:
-        outliers = np.abs(stats.zscore(number_of_nans_in_columns)) > 1
-        columns_to_drop = X.columns[outliers]
-        if dont_drop:
-            columns_to_drop = [column for column in columns_to_drop if column not in dont_drop]
-            X = X.drop(columns_to_drop, axis=1)
-
-    X = X.dropna()
-
-    return X
-
-def distribution(values, title='Distribution of values', log_scale=False):
-    unique, counts = np.unique([str(val) for val in values], return_counts=True)
-    distribution = pd.DataFrame({'Count': counts, 'Values': unique})
-
-    fig = go.Figure(data=[go.Bar(x=distribution['Values'], y=distribution['Count'], text=distribution['Count'], textposition='auto')])
-    if log_scale:
-        fig.update_layout(title=title, xaxis=dict(title='Values'), yaxis=dict(title='Count'), yaxis_type="log")
-    else:
-        fig.update_layout(title=title, xaxis=dict(title='Values'), yaxis=dict(title='Count'))
-    fig.show()
 
 def string_format(val):
     if not isinstance(val, str):
         res = np.nan
     else:
-        res = ''
+        res = ""
         for c in val:
             if c in string.digits:
-                res += 'D'
+                res += "D"
             elif c in string.ascii_letters:
-                res += 'L'
+                res += "L"
             elif c in string.punctuation:
-                res += 'P'
+                res += "P"
             elif c in string.whitespace:
-                res += 'S'
+                res += "S"
             else:
-                res += 'O'
+                res += "O"
     return res
+
 
 def dist(x, normalize=False):
     u, c = np.unique(x, return_counts=True)
     if normalize:
         total = np.sum(c)
         c = c / total
-    return pd.DataFrame({'value': u, 'freq': c})
+    return pd.DataFrame({"value": u, "freq": c})
--- tools.py
+++ tools.py
@@ -1,45 +1,5 @@
 import os
 
-import numpy as np
-import pandas as pd
-import time
-
-import datetime
-
-
 
 def clear_terminal():
-    os.system('cls' if os.name=='nt' else 'clear')
-
-def parent_dir(path):
-    if path[-1] == '\\':
-        path = path[:-1]
-    path = path.split('\\')
-    path = path[:-1]
-    path = '\\'.join(path) + '\\'
-    return path
-
-# Medium article "Dynamically add a method to a class" by Michael Garod
-# https://medium.com/@mgarod/dynamically-add-a-method-to-a-class-in-python-c49204b85bd6
-def add_method(cls):
-    '''
-    Add a method to a class.
-
-    :Example: 
-    # Adding method foo() to class 
-    @add_method(A)
-    def foo():
-        print('hello world!')
-    # Method foo() can still be used on its own (doesn't need self attribute).
-    '''
-    def decorator(func):
-        @wraps(func) 
-        def wrapper(self, *args, **kwargs): 
-            return func(*args, **kwargs)
-        setattr(cls, func.__name__, wrapper)
-        # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
-        return func # returning func means func can still be used normally
-    return decorator
-
-def elapsed_time(t0):
-    return str(datetime.timedelta(seconds=time.time() - t0))[:-7]
+    os.system("cls" if os.name == "nt" else "clear")
