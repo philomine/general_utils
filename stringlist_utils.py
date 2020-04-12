82471d3a8e36440ad7392fb63ab70eda67a43502
Philomene Bobichon <philomene.bobichon@konecranes.com>
2020-04-12 19:16:28 +0200

    Getting WIP back

--- stringlist_utils.py
+++ stringlist_utils.py
@@ -1,31 +1,27 @@
 import numpy as np
 import pandas as pd
-
 from .data_analysis import dist
 
-
 def stringlist_length(stringlist):
-    if isinstance(stringlist, str) and (stringlist != ""):
-        length = len(stringlist.split(";"))
+    if isinstance(stringlist, str) and (stringlist != ''):
+        length = len(stringlist.split(';'))
     else:
         length = 0
     return length
 
-
 def append_stringlists(stringlists):
     res = []
     for stringlist in stringlists:
-        if isinstance(stringlist, str) and (stringlist != ""):
-            res += stringlist.split(";")
+        if isinstance(stringlist, str) and (stringlist != ''):
+            res += stringlist.split(';')
     return res
-
 
 def stringlists_dist(stringlists, normalize=True):
     values = np.unique(append_stringlists(stringlists))
     res = pd.Series(index=values, data=0)
     for stringlist in stringlists:
-        if isinstance(stringlist, str) and (stringlist != ""):
-            stringlist_dist = dist(stringlist.split(";"), normalize=normalize)
-            stringlist_dist = stringlist_dist.set_index("value").freq
+        if isinstance(stringlist, str) and (stringlist != ''):
+            stringlist_dist = dist(stringlist.split(';'), normalize=normalize)
+            stringlist_dist = stringlist_dist.set_index('value').freq
             res[stringlist_dist.index] += stringlist_dist.values
-    return pd.DataFrame({"value": res.index, "freq": res.values})
+    return pd.DataFrame({'value': res.index, 'freq': res.values})
--- vizualisation.py
+++ vizualisation.py
@@ -1,16 +1,15 @@
-import numpy as np
-import pandas as pd
+import numpy as np 
+import pandas as pd 
+
 import plotly
+import plotly.graph_objects as go 
 import plotly.express as px
-import plotly.graph_objects as go
 
 from .data_analysis import dist
-
 
 def save_fig(fig, filename=None):
     if filename is not None:
         plotly.offline.plot(fig, filename=filename, auto_open=False)
-
 
 def dist_pie_chart(dist, filename=None, other_cat=True):
     labels = dist.value
@@ -21,72 +20,56 @@ def dist_pie_chart(dist, filename=None, other_cat=True):
         total = np.sum(values)
         other_cat = (values / total) < 0.01
         if np.any(other_cat):
-            labels = list(labels[~other_cat]) + [
-                f"Other ({np.sum(other_cat)})"
-            ]
+            labels = list(labels[~other_cat]) + [f'Other ({np.sum(other_cat)})'] 
             other_values = np.sum(values[other_cat])
             values = list(values[~other_cat]) + [other_values]
-    fig = go.Figure(
-        data=[go.Pie(labels=labels, values=values, textinfo="label+percent")]
-    )
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent')])
     fig.update_layout(
-        annotations=[
-            go.layout.Annotation(
-                x=1,
-                y=1,
-                showarrow=False,
-                text=f"Total number of values: {round(np.sum(values), 0)}",
-                xref="paper",
-                yref="paper",
-                align="right",
-            )
-        ]
+        annotations=[go.layout.Annotation(
+            x=1, y=1,
+            showarrow=False,
+            text=f"Total number of values: {round(np.sum(values), 0)}",
+            xref="paper",
+            yref="paper",
+            align="right"
+        )]
     )
     save_fig(fig, filename=filename)
     return fig
-
 
 def sample_pie_chart(sample, filename=None, other_cat=True):
     sample_dist = dist(sample)
     return dist_pie_chart(sample_dist, filename=filename, other_cat=other_cat)
 
-
 def dist_bar_chart(dist, filename=None):
     labels = dist.value
     values = dist.freq
-    fig = go.Figure(
-        [go.Bar(x=labels, y=values, text=values, textposition="auto")]
-    )
+    fig = go.Figure([go.Bar(x=labels, y=values, text=values, textposition='auto')])
     fig.update_layout(
-        annotations=[
-            go.layout.Annotation(
-                x=1,
-                y=1.05,
-                showarrow=False,
-                text=f"Total number of values: {round(np.sum(values), 0)}",
-                xref="paper",
-                yref="paper",
-                align="right",
-            )
-        ]
+        annotations=[go.layout.Annotation(
+            x=1, y=1.05,
+            showarrow=False,
+            text=f"Total number of values: {round(np.sum(values), 0)}",
+            xref="paper",
+            yref="paper",
+            align="right"
+        )]
     )
     save_fig(fig, filename=filename)
     return fig
-
 
 def sample_bar_chart(sample, filename=None):
     sample_dist = dist(sample)
     return dist_bar_chart(sample_dist, filename=filename)
 
-
 def scatter_plot(X, labels=None, filename=None):
     if labels:
-        color = "color"
-        X = pd.DataFrame(X, columns=["x", "y"])
-        X["color"] = labels
+        color = 'color'
+        X = pd.DataFrame(X, columns=['x', 'y'])
+        X['color'] = labels 
     else:
         color = None
-        X = pd.DataFrame(X, columns=["x", "y"])
-    fig = px.scatter(X, x="x", y="y", color=color)
+        X = pd.DataFrame(X, columns=['x', 'y'])
+    fig = px.scatter(X, x='x', y='y', color=color)
     save_fig(fig, filename=filename)
     return fig
